46b7ce83f9aa21dde35c66a5d4c626a38b3d83ef
박래인 <parkrain1189@gmail.com>
2023-10-14 00:33:09 +0900

    change car stuff to car dict

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ pygame.display.set_icon(logo)
 pygame.display.set_caption("Drifting Car")
 
 # 240 x 180 사이즈의 스크린 표면을 만듦
-screen = pygame.display.set_mode((600, 600))
+SCREEN_SIZE = (600,600)
+screen = pygame.display.set_mode(SCREEN_SIZE)
 
 # 자동차 로드
-car_original = pygame.image.load('car.png')
-car_x = screen.get_size()[0] / 2
-car_y = screen.get_size()[1] / 2
-car_v_x = 0
-car_v_y = 0
-car_a = 0.0005
-car_r = 0
-car_r_a = 0.1
-car_f = 1.001
+car = {
+    'original_image': pygame.image.load('car.png'),
+    'x': SCREEN_SIZE[0] / 2,
+    'y': SCREEN_SIZE[1] / 2,
+    'r': 0,
+    'v': {
+        'x': 0,
+        'y': 0,
+        'r': 0.1
+    },
+    'a': 0.0005,
+    'ff': 1.001
+}
 
 # 메인 루프를 제어할 변수 정의
 running = True
@@ -28,13 +33,16 @@ running = True
 # 메인 루프
 while running:
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        car_r += car_r_a
-    if keys[pygame.K_RIGHT]:
-        car_r -= car_r_a
     if keys[pygame.K_UP]:
-        car_v_y += car_a * math.sin(math.radians(car_r))
-        car_v_x += car_a * math.cos(math.radians(car_r))
+        car['v']['x'] += car['a'] * math.cos(math.radians(car['r']))
+        car['v']['y'] += car['a'] * math.sin(math.radians(car['r']))
+    if keys[pygame.K_DOWN]:
+        car['v']['x'] -= car['a'] * math.cos(math.radians(car['r']))
+        car['v']['y'] -= car['a'] * math.sin(math.radians(car['r']))
+    if keys[pygame.K_LEFT]:
+        car['r'] += car['v']['r']
+    if keys[pygame.K_RIGHT]:
+        car['r'] -= car['v']['r']
 
     # 이벤트 핸들러, 이벤트 큐로부터 모든 이벤트를 얻는다.
     for event in pygame.event.get():
@@ -43,15 +51,16 @@ while running:
             # 메인 루프를 탈출하기 위해 변수를 False 로 바꾼다.
             running = False
 
-    car_v_x /= car_f
-    car_v_y /= car_f
-    car_x += car_v_x
-    car_y -= car_v_y
-    car = pygame.transform.rotate(car_original, car_r)
-    car_rect = car.get_rect(center = (car_x, car_y))
+    car['v']['x'] /= car['ff']
+    car['v']['y'] /= car['ff']
+    car['x'] += car['v']['x']
+    car['y'] -= car['v']['y']
+
+    car['image'] = pygame.transform.rotate(car['original_image'], car['r'])
+    car['rect'] = car['image'].get_rect(center = (car['x'], car['y']))
 
     screen.fill((0,0,0))
-    screen.blit(car, car_rect)
+    screen.blit(car['image'], car['rect'])
 
     pygame.display.update()
 
